7b3ddbcde6b2864d8f35eae1c2e9e411fd8778b4
Nate Wang <supnate@gmail.com>
2017-02-09 00:18:21 +0800

    Improve demo about dialog.

--- src/features/home/About.js
+++ src/features/home/About.js
@@ -46,38 +46,50 @@ export class About extends Component {
           <h1>Welcome to the Rekit Portal demo!</h1>
           <p className="version">Version: v{rekit.portalVersion}</p>
         </div>
-
         <p>
           This is a demo of Rekit and Rekit portal for you to quickly learn how Rekit helps to creating a scalable web application.
         </p>
-        <br />
         <p>
           Rekit portal itself is also created by Rekit. It&apos;s a web application for managing a Rekit project. It not only provides web UIs for creating/moving/deleting elements of a Rekit app, but also provides tools for analyzing/building/testing a Rekit application.
         </p>
-        <br />
         <h3>Create your own Rekit app with 3 only steps!</h3>
         <p>
           Though this demo is readonly, you can try a full-featured Rekit portal by creating your own Rekit app. It&apos;s super easy!
         </p>
         <br />
         <Steps>
-          <Step status="process" title="Install rekit" description={<ul><li><span>&gt;</span> npm install rekit -g</li></ul>} />
+          <Step status="process" title="Install rekit" description={<ul><li><span>&gt;</span> npm install rekit@next -g</li></ul>} />
           <Step status="process" title="Create app" description={<ul><li><span>&gt;</span> rekit create app1</li><li><span>&gt;</span> cd app1</li><li><span>&gt;</span> npm install</li></ul>} />
           <Step status="process" title="Start the app" description={<ul><li><span>&gt;</span> npm start</li></ul>} />
         </Steps>
-        <br />
-        <p>* Alternatively you can use <a href="https://yarnpkg.com/" target="_blank">Yarn</a> as the package manager instead of npm.</p>
-        <br />
+        <p>Then access <a href="http://localhost:6075" target="_blank">http://localhost:6075</a> for your app. <a href="http://localhost:6076" target="_blank">http://localhost:6076</a> for rekit portal.</p>
+        <p>* Alternatively you can use <a href="https://yarnpkg.com/" target="_blank">Yarn</a> as the package manager rather than npm.</p>
+        <h3>Rekit portal architecture</h3>
+        <p className="architecture">
+          Rekit portal is a web app built with Rekit, it uses Rekit&apos;s <a href="#">feature oriented architecture</a>. Actually Rekit portal is built up with 5 well decoupled features:
+          <ol>
+            <li><b>Common</b>: provides common utilities, components for all features.</li>
+            <li><b>Home</b>: the foundation feature of Rekit portal, including managing the meta data of the project, main UI layout, project explorer etc.</li>
+            <li><b>Diagram</b>: visualize the project architecture.</li>
+            <li><b>Rekit cmds</b>: provides actions and components for communicating with backend server to create/rename/move/delete Rekit elements like components.</li>
+            <li><b>Rekit tools</b>: provides pages to test or build the project.</li>
+          </ol>
+        </p>
+        <p>
+          From the overview diagram in dashboard, you can have a intuitive view of how features work together.
+        </p>
+        <p>
+          Besides the web UI, Rekit portal also includes an express middleware to use APIs provided by Rekit core to manage the project.
+        </p>
         <h3>Learn more</h3>
         <p className="learn-more">
           <ul>
-            <li><a href="#">Rekit 2.0 is out with nice new features!</a></li>
-            <li><a href="#">Rekit docs: rekit.js.org</a></li>
-            <li><a href="#">Feature oriented web development with React, Redux and React-router</a></li>
-            <li><a href="#">A new approach to managing Redux actions</a></li>
+            <li><a href="#" target="_blank">Rekit 2.0 is out with nice new features!</a></li>
+            <li><a href="http://rekit.js.org" target="_blank">Rekit docs: rekit.js.org</a></li>
+            <li><a href="#" target="_blank">Feature oriented web development with React, Redux and React-router</a></li>
+            <li><a href="https://medium.com/@nate_wang/a-new-approach-for-managing-redux-actions-91c26ce8b5da" target="_blank">A new approach to managing Redux actions</a></li>
           </ul>
         </p>
-        <br />
         <p>
           Any more questions or feedback? Please visit: <a href="https://github.com/supnate/rekit" target="_blank">https://github.com/supnate/rekit</a>
         </p>
